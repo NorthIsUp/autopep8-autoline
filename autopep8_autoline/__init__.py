c70aec9d5ef28f83b69ed35882e9ec395de3e5bf
Adam Hitchcock <adam@northisup.com>
2018-06-07 14:58:38 -0700

    add autodeploy

--- autopep8_autoline/__init__.py
+++ autopep8_autoline/__init__.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 """Top-level package for autopep8-autoline."""
 
 __author__ = """Adam Hitchcock"""
--- tests/test_autopep8_autoline.py
+++ tests/test_autopep8_autoline.py
@@ -6,17 +6,6 @@
 
 import unittest
 
-from autopep8_autoline import autopep8_autoline
 
-
-class TestAutopep8_autoline(unittest.TestCase):
-    """Tests for `autopep8_autoline` package."""
-
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
+def test_import():
+    import autopep8_autoline
